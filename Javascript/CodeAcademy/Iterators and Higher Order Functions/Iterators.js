57eb21e98e8d7328208843bdae31af4e99b040f8
Animesh Paul <ani.nitmz@gmail.com>
2020-09-01 01:00:30 -0600

    Changed const to var

--- Javascript/CodeAcademy/Iterators and Higher Order Functions/Iterators.js
+++ Javascript/CodeAcademy/Iterators and Higher Order Functions/Iterators.js
@@ -1,20 +1,20 @@
-const artists = ['Picasso', 'Kahlo', 'Matisse', 'Utamaro'];
+var artists = ['Picasso', 'Kahlo', 'Matisse', 'Utamaro'];
 
 artists.forEach(artist => {
   console.log(artist + ' is one of my favorite artists.');
 });
 
-const numbers = [1, 2, 3, 4, 5];
+var numbers = [1, 2, 3, 4, 5];
 
-const squareNumbers = numbers.map(number => {
+var squareNumbers = numbers.map(number => {
   return number * number;
 });
 
 console.log(squareNumbers);
 
-const things = ['desk', 'chair', 5, 'backpack', 3.14, 100];
+var things = ['desk', 'chair', 5, 'backpack', 3.14, 100];
 
-const onlyNumbers = things.filter(thing => {
+var onlyNumbers = things.filter(thing => {
   return typeof thing === 'number';
 });
 
@@ -22,55 +22,55 @@ console.log(onlyNumbers);
 
 console.log("***************************");
 
-const fruits = ['mango', 'papaya', 'pineapple', 'apple'];
+var fruits = ['mango', 'papaya', 'pineapple', 'apple'];
 
 // Iterate over fruits below
 fruits.forEach(fruit => console.log('I want to eat a '+fruit));
 
 console.log("***************************");
 
-const animals = ['Hen', 'elephant', 'llama', 'leopard', 'ostrich', 'Whale', 'octopus', 'rabbit', 'lion', 'dog'];
+var animals = ['Hen', 'elephant', 'llama', 'leopard', 'ostrich', 'Whale', 'octopus', 'rabbit', 'lion', 'dog'];
 
 // Create the secretMessage array below
-const secretMessage = animals.map(animal => animal[0]);
+var secretMessage = animals.map(animal => animal[0]);
 
 console.log(secretMessage.join(''));
 
-const bigNumbers = [100, 200, 300, 400, 500];
+var bigNumbers = [100, 200, 300, 400, 500];
 
 function divide(num){
   return num/100;
 }
 // Create the smallNumbers array below
-const smallNumbers = bigNumbers.map(divide);
+var smallNumbers = bigNumbers.map(divide);
 console.log(smallNumbers);
 
 console.log("***************************");
 
-const randomNumbers = [375, 200, 3.14, 7, 13, 852];
+var randomNumbers = [375, 200, 3.14, 7, 13, 852];
 
 // Call .filter() on randomNumbers below
-const smallNumbers = randomNumbers.filter(number => number < 250);
+smallNumbers = randomNumbers.filter(number => number < 250);
 
-const favoriteWords = ['nostalgia', 'hyperbole', 'fervent', 'esoteric', 'serene'];
+var favoriteWords = ['nostalgia', 'hyperbole', 'fervent', 'esoteric', 'serene'];
 
 
 // Call .filter() on favoriteWords below
-const longFavoriteWords = favoriteWords.filter(word => word.length > 7);
+var longFavoriteWords = favoriteWords.filter(word => word.length > 7);
 
 console.log("***************************");
 
-const animals = ['hippo', 'tiger', 'lion', 'seal', 'cheetah', 'monkey', 'salamander', 'elephant'];
+animals = ['hippo', 'tiger', 'lion', 'seal', 'cheetah', 'monkey', 'salamander', 'elephant'];
 
-const foundAnimal = animals.findIndex(animal => animal === 'elephant');
+var foundAnimal = animals.findIndex(animal => animal === 'elephant');
 
-const startsWithS = animals.findIndex(animal => animal[0] === 's');
+var startsWithS = animals.findIndex(animal => animal[0] === 's');
 
 console.log("***************************");
 
-const newNumbers = [1, 3, 5, 7];
+var newNumbers = [1, 3, 5, 7];
 
-const newSum = newNumbers.reduce((accumulator, currentValue) => {
+var newSum = newNumbers.reduce((accumulator, currentValue) => {
   console.log('The value of accumulator: ', accumulator);
   console.log('The value of currentValue: ', currentValue);
   return accumulator + currentValue;
@@ -81,7 +81,7 @@ console.log(newSum);
 
 console.log("***************************");
 
-const words = ['unique', 'uncanny', 'pique', 'oxymoron', 'guise'];
+var words = ['unique', 'uncanny', 'pique', 'oxymoron', 'guise'];
 
 // Something is missing in the method call below
 
@@ -89,7 +89,7 @@ console.log(words.some(word => {
   return word.length < 6}));
 
 // Use filter to create a new array
-const interestingWords = words.filter(word => word.length > 5);
+var interestingWords = words.filter(word => word.length > 5);
 
 
 // Make sure to uncomment the code below and fix the incorrect code before running it
@@ -98,25 +98,25 @@ console.log(interestingWords.every(word => word.length > 5));
 
 console.log("***************************");
 
-const cities = ['Orlando', 'Dubai', 'Edinburgh', 'Chennai', 'Accra', 'Denver', 'Eskisehir', 'Medellin', 'Yokohama'];
+var cities = ['Orlando', 'Dubai', 'Edinburgh', 'Chennai', 'Accra', 'Denver', 'Eskisehir', 'Medellin', 'Yokohama'];
 
-const nums = [1, 50, 75, 200, 350, 525, 1000];
+var nums = [1, 50, 75, 200, 350, 525, 1000];
 
 //  Choose a method that will return undefined
 cities.forEach(city => console.log('Have you visited ' + city + '?'));
 
 // Choose a method that will return a new array
-const longCities = cities.filter(city => city.length > 7);
+var longCities = cities.filter(city => city.length > 7);
 
 // Choose a method that will return a single value
-const word = cities.reduce((acc, currVal) => {
+var word = cities.reduce((acc, currVal) => {
   return acc + currVal[0]
 }, "C");
 
 console.log(word)
 
 // Choose a method that will return a new array
-const smallerNums = nums.map(num => num - 5);
+var smallerNums = nums.map(num => num - 5);
 
 // Choose a method that will return a boolean value
 nums.some(num => num < 0);
